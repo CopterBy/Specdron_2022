0c5bdfc989855624fc003296dc550a6b5b7864e0
Max <96985345+CopterBy@users.noreply.github.com>
2022-11-18 16:02:10 +0300

    добавил detail и index

--- specdron_django/project/models.py
+++ specdron_django/project/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+# python manage.py makemigrations project # создание файла БД в папке migrations
+# python manage.py migrate project # выполнение миграции данных для приложения project
+# python manage.py migrate # выполнение миграции данных для всего проекта
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=50)
+    image = models.FileField(upload_to='img/')
